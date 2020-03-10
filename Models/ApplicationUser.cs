bdbe4b125570e0ff3c97ff21b0aca3fee81b0e06
Yaroslav Kibysh <yaroslavki@tahometer.com>
2020-03-10 12:23:13 +0200

    Initial setup

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_todo.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
